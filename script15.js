cf2e90eb223db5f5c8c3b6a0ed0876fcec100244
ruslantuz <ruslantuz10@gmail.com>
2025-05-08 22:28:19 +0300

    lab 15 add

---
+++ script15.js
@@ -0,0 +1,28 @@
+
+alert("Привіт!");
+if(confirm("Ви бажаєте відкрити попередню лабораторну роботу?")){
+    window.open("lab13.html","displaywind", "width=500, height=400, left=200px, top=100px, status=no, location=no, menubar=no");
+}
+
+function reg(obj){
+    obj.regt.value=prompt("Введіть Ваше ім'я",""); 
+}
+
+document.getElementById("regbtn").onclick = () => {
+    reg(form1);
+    return false;
+}
+
+
+function closeWindow(){
+    if(confirm("Ви справді бажаєте закрити вікно?")){
+        window.close();
+    }
+}
+
+// document.getElementById("closebtn").onclick = () => {
+//     if(confirm("Ви справді бажаєте закрити вікно?")){
+//         wind.close();
+//         return false;
+//     }
+// };
