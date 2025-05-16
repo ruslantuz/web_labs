b0f3f6494b8e6b1220bf0be3a1bb269551023d73
ruslantuz <ruslantuz10@gmail.com>
2025-05-16 11:54:11 +0300

    completed lab18

--- script18.js
+++ script18.js
@@ -1,18 +1,28 @@
 var calcBtn = document.getElementById("calcBtn");
 
-
 var result = document.getElementById("result");
 
-
-
-
-
-
-
-
 calcBtn.onclick = ()=>{
-    console.log(form1.calcOption.checked);
-
-
+    var selectedOption = document.querySelector(`input[name=calcOption]:checked`).value;
+    var radius = form1.radius.value;
+    if(radius < 0){
+        result.textContent = "Введіть додатнє значення!";
+        return 1;
+    }
+    
+    switch (selectedOption){
+        case ("square"):
+            result.textContent = "Площа круга: " + Math.PI * Math.pow(radius,2);
+            break;
+        case("length"):
+            result.textContent = "Довжина кола: " + 2 * Math.PI * radius;
+            break;
+        case("volume"):
+            result.textContent = "Об'єм кулі: " + 4/3 * Math.PI * Math.pow(radius,3);
+            break;
+        case("surface"):
+            result.textContent = "Площа поверхні сфери: " + 4 * Math.PI *  Math.pow(radius,2);
+            break;
+    }
 }
 
